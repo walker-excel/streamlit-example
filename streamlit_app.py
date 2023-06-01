e4586fc1c5285400c02dfbfd8bb35edc0545cea0
walker-excel <111059631+walker-excel@users.noreply.github.com>
2023-06-01 14:02:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ def main():
         if api_key and request_url and orgcode:
             # Set the API key and orgcode in the headers
             headers = {
-                "accept": "application/jason",
+                "accept": "application/json",
                 "apikey": api_key,
                 "orgcode": orgcode
             }
