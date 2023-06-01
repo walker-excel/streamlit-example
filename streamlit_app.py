3f9df9acd8013c92cf1da35bdbd8e90b5ee79392
walker-excel <111059631+walker-excel@users.noreply.github.com>
2023-06-01 14:11:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import requests
 import streamlit as st
 
 # Function to make API requests
-def make_api_request(url, headers=None):
-    response = requests.get(url, headers=headers)
+def make_api_request(url, headers=None, data=None):
+    response = requests.post(url, headers=headers, json=data)
     response.raise_for_status()
     return response.json()
 
@@ -19,30 +19,37 @@ def main():
     # Input API key
     api_key = st.text_input("Enter your API key", type="password")
 
-    # Input request URL
-    request_url = st.text_input("Enter the request URL")
-
     # Input orgcode
     orgcode = st.text_input("Enter the orgcode")
 
     # Button to trigger API request
     if st.button("Fetch Data"):
-        if api_key and request_url and orgcode:
+        if api_key and orgcode:
             # Set the API key and orgcode in the headers
             headers = {
                 "accept": "application/json",
                 "apikey": api_key,
-                "orgcode": orgcode
+                "orgcode": orgcode,
+                "Content-Type": "application/json"
+            }
+
+            # Prepare the request data
+            request_data = {
+                "pageNumber": 1,
+                "pageSize": 10,
+                "includeInactive": True,
+                "sortParameters": [{"fieldName": "string", "isAscending": True}],
+                "filterParameters": [{"fieldName": "string", "fieldValue": "string", "isLikeSearch": True}]
             }
 
             try:
                 # Make API request to the provided URL
-                response = make_api_request(request_url, headers=headers)
+                response = make_api_request("https://restapina.ticketsearch.com/venue/api/v1/venues", headers=headers, data=request_data)
                 st.json(response)
             except requests.exceptions.HTTPError as e:
                 st.error(f"Error: {e}")
         else:
-            st.warning("Please enter your API key, request URL, and orgcode.")
+            st.warning("Please enter your API key and orgcode.")
 
 # Run the Streamlit app
 if __name__ == "__main__":
