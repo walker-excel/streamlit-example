7ccfde1c897d756847fd431ba1c7e000fc3069d8
walker-excel <111059631+walker-excel@users.noreply.github.com>
2023-06-01 15:15:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ def main():
         if api_key and orgcode:
             # Set the API key and orgcode in the headers
             headers = {
-                "Content-Type : application/json",
-                f"apikey: {api_key}",
-                f"orgcode: {orgcode}"
+                "Content-Type": "application/json",
+                "apikey": api_key,
+                "orgcode": orgcode
             }
 
             headers_data= json.dumps(headers)
